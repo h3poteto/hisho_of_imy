2a19edb0e0ae0779a9c3bcb48e12047daa4fe4d3
h3poteto <h3.poteto@gmail.com>
2013-10-13 01:23:42 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HishoOfImy::Application.config.secret_token = '682e188ed91279c5e5068067e50635bb110836500c467de11c51e52ad6de15ecae908b53b231dfdf8ec494c17821efa73bc94397ded88d707c99fc1490ef15b5'
